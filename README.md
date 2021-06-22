e056b53bb840604b9f45657ec43e07e3027d3b38
LiLongwei <lonelyholiday@qq.com>
2021-06-22 18:44:18 +0800

    optimization Library Used

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # phyota
 PHY Bluetooth Low Energy OTA Library
+
+## 使用步骤
+
+### 一、添加依赖
+
+Step 1. 项目工程的build.gradle，添加jitpack库
+
+```groovy
+allprojects {
+		repositories {
+			maven { url 'https://jitpack.io' }
+		}
+}
+```
+  
+Step 2. 模块的build.gradle，添加ota依赖库
+
+```groovy
+dependencies {
+	   implementation 'com.github.lilongweidev:phyota:1.0.0'
+}
+```
+
+### 二、功能使用
+
+库中提供了蓝牙ble扫描和连接，文件解析，ota升级，以及一些其他的工具类弹窗。
+
+
---
+++ phy_ota_sdk/src/main/java/com/phy/ota/sdk/dialog/DialogHelper.java
@@ -0,0 +1,47 @@
+package com.phy.ota.sdk.dialog;
+
+import android.content.Context;
+
+import com.phy.ota.sdk.R;
+import com.phy.ota.sdk.utils.SizeUtils;
+
+public class DialogHelper {
+
+    //加载弹窗
+    private static CustomDialog loadingDialog;
+    //升级固件结果弹窗
+    private static AlertDialog updateResultDialog;
+
+    /**
+     * 显示加载弹窗
+     */
+    public static void showLoading(Context context) {
+        loadingDialog = new CustomDialog(context);
+        loadingDialog.show();
+    }
+
+
+    /**
+     * 隐藏加载弹窗
+     */
+    public static void hideLoading() {
+        if (loadingDialog != null) {
+            loadingDialog.dismiss();
+        }
+    }
+
+    /**
+     * 升级结果弹窗
+     */
+    public static void showUpdateResultDialog(Context context, boolean isSuccess, int code) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .addDefaultAnimation()
+                .setContentView(R.layout.dialog_update_result)
+                .setCancelable(true)
+                .setIcon(R.id.iv_update_result, isSuccess ? R.mipmap.ic_success : R.mipmap.ic_failure)
+                .setText(R.id.tv_update_result, isSuccess ? "升级成功" : "失败：" + code)
+                .setWidthAndHeight(SizeUtils.dp2px(context, 120), SizeUtils.dp2px(context, 120));
+        updateResultDialog = builder.create();
+        updateResultDialog.show();
+    }
+}
